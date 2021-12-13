190a957c5cdf46fb606a8a7580314398520946e5
jetlime <paul.houssel@yahoo.de>
2021-12-13 16:26:24 +0100

    debug

--- source/app.js
+++ source/app.js
@@ -281,7 +281,7 @@ app.get("/", (req, res) => {
         let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
         console.log(ip)
         createCookie(ip).then((result) => {
-            console.log(result)
+            console.log(Object.keys(result))
             res.cookie("user_session", result.id)
             res.render("home", {
                 failure: failure,
